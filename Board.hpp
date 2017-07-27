dbd9fcd9251c52e1f6fb1764aabd215b0c6be6bb
Tommy <tommy.russoniello@gmail.com>
2017-07-26 23:47:08 -0400

    Optimize main algorithm *BUG*

--- Board.hpp
+++ Board.hpp
@@ -1,28 +1,37 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 using std::cout;
 using std::endl;
+using std::vector;
+using std::find;
+//using std::push_back;
 
 class Board
 {
  public :
-  char topL, topM, topR, midL, midM, midR, botL, botM, botR;
+  bool won;
+  char topleft, topmid, topright, midleft, midmid, midright, botleft, botmid, botright;
+  vector<int> movesX;
+  vector<int> movesO;
 
   Board ()
     {
-      topL = topM = topR = midL = midM = midR = botL = botM = botR = ' ';
+      topleft = topmid = topright = midleft = midmid = midright = botleft = botmid = botright = ' ';
+      won = false;
     }
 
 void
 print ()
 {
-  cout << endl << " " << topL << " | " << topM << " | " << topR << "  " << endl
+  cout << endl << " " << topleft << " | " << topmid << " | " << topright << "  " << endl
        << "___|___|___" << endl
-       << " " << midL << " | " << midM << " | " << midR << " " << endl
+       << " " << midleft << " | " << midmid << " | " << midright << " " << endl
        << "___|___|___" << endl
-       << " " << botL << " | " << botM << " | " << botR << " " << endl
+       << " " << botleft << " | " << botmid << " | " << botright << " " << endl
        << "   |   |   " << endl << endl;
 }
 
@@ -38,33 +47,96 @@ printSample ()
 }
 
   bool
-  setValue (int row, int column, char xo)
+  setValue (int pos, char xo)
   {
-    switch(row)
+    switch(pos)
       {
-      case(1):
-	switch(column){
-	case(1): if(topL != ' '){break;} topL = xo; return true;
-	case(2): if(topM != ' '){break;} topM = xo; return true;
-	case(3): if(topR != ' '){break;} topR = xo; return true;
-	} break;
-      case(2):
-	switch(column){
-	case(1):if(midL != ' '){break;} midL = xo; return true;
-	case(2):if(midM != ' '){break;} midM = xo; return true;
-	case(3):if(midR != ' '){break;} midR = xo; return true;
-	} break;
-      case(3):
-	switch(column){
-	case(1):if(botL != ' '){break;} botL = xo; return true;
-	case(2):if(botM != ' '){break;} botM = xo; return true;
-	case(3):if(botR != ' '){break;} botR = xo; return true;
-	} break;
+        case(7): return update(topleft, xo, 1);
+        case(8): return update(topmid, xo, 2);
+        case(9): return update(topright, xo, 3);
+        case(4): return update(midleft, xo, 4);
+        case(5): return update(midmid, xo, 5);
+        case(6): return update(midright, xo, 6);
+        case(1): return update(botleft, xo, 7);
+        case(2): return update(botmid, xo, 8);
+        case(3): return update(botright, xo, 9);
+        default: return false; break;
       }
-    return false;
   }
 
 
  private :
-  //none
+  
+  bool
+  update (char& pos, char xo, int posNum)
+  {
+    if (pos != ' ') { return false; }
+
+    topleft = xo;
+
+    if (xo == 'X')
+    {
+      movesX.push_back(posNum);
+      if (movesX.size() >= 3) { hasWon(posNum, movesX); }
+    }
+    else
+    {
+      movesO.push_back(posNum);
+      if (movesO.size() >= 3) { hasWon(posNum, movesO); }
+    }
+    return true;
+  }
+
+  void
+  hasWon (int pos, vector<int>& moves)
+  {
+    if      (!pos & 1) { midCheck (pos, moves);   }
+    else if (pos != 5) { cornerCheck(pos, moves); }
+    else               { centerCheck(moves); }
+  }
+
+  void
+  midCheck (int pos, vector<int>& moves)
+  {
+    won = (
+      (vfind (moves, pos - 1) && vfind (moves, pos + 1)) ||
+      (vfind (moves, pos - 3) && vfind (moves, pos + 3)) ||
+      (vfind (moves, 5) && vfind (moves, opposite(pos)))
+    );
+  }
+
+  void
+  cornerCheck (int pos, vector<int>& moves)
+  {
+    won = (
+      (vfind (moves, pos - 1) && vfind (moves, pos - 2)) ||
+      (vfind (moves, pos + 1) && vfind (moves, pos + 2)) ||
+      (vfind (moves, pos - 3) && vfind (moves, pos - 6)) ||
+      (vfind (moves, pos + 3) && vfind (moves, pos + 6)) ||
+      (vfind (moves, 5) && vfind (moves, opposite(pos)))
+    );
+  }
+
+  void
+  centerCheck (vector<int>& moves)
+  {
+    won = true;
+    for (int i = 1; i <= 4; ++i)
+    {
+      if (vfind (moves, 5 - i) && vfind (moves, 5 + i)) { return; }
+    }
+    won = false;
+  }
+
+  int
+  opposite (int pos)
+  {
+     return 5 - (pos - 5);
+  }
+
+  bool
+  vfind (vector<int> vec, int num)
+  {
+    return (vec.end() != find (vec.begin(), vec.end(), num));
+  }
 };
--- tictactoe.cpp
+++ tictactoe.cpp
@@ -17,7 +17,6 @@ void hasWon ();
 
 Board board;
 char player = 'X';
-string error = "Command Not Recognized\n";
 
 int
 main ()
@@ -65,7 +64,7 @@ draw (string option)
 void
 update ()
 {
-  int rowNum, colNum;
+  int position;
   string input;
   char move;
   while (true)
@@ -76,23 +75,25 @@ update ()
         move = input[0];
         switch (move)
         {
-          case '7' : rowNum = 1; colNum = 1;  break;
-          case '8' : rowNum = 1; colNum = 2;  break;
-          case '9' : rowNum = 1; colNum = 3;  break;
-          case '4' : rowNum = 2; colNum = 1;  break;
-          case '5' : rowNum = 2; colNum = 2;  break;
-          case '6' : rowNum = 2; colNum = 3;  break;
-          case '1' : rowNum = 3; colNum = 1;  break;
-          case '2' : rowNum = 3; colNum = 2;  break;
-          case '3' : rowNum = 3; colNum = 3;  break;
-          case 'q' : exit(EXIT_SUCCESS);      break;
-          case 'h' : draw ("help");        continue;
-          default  : cout << error;        continue;
+          case '1' : break;
+          case '2' : break;
+          case '3' : break;
+          case '4' : break;
+          case '5' : break;
+          case '6' : break;
+          case '7' : break;
+          case '8' : break;
+          case '9' : break;
+
+          case 'q' : exit(EXIT_SUCCESS); break;
+          case 'h' : draw ("help");      continue;
+
+          default  : cout << "Command Not Recognized\n"; continue;
         }
       break;
     }
 
-    if(!board.setValue(rowNum, colNum, player))
+    if(!board.setValue(move, player))
     {
       draw ();
       cout << "Space already taken\n";
@@ -114,17 +115,22 @@ updatePlayer ()
 void
 hasWon()
 {
-  if(board.topL+board.topM+board.topR==264||board.midL+board.midM+board.midR==264||board.botL+board.botM+
-     board.botR==264||board.topR+board.midR+board.botR==264||board.topM+board.midM+board.botM==264||
-     board.topL+board.midL+board.botL==264||board.topR+board.midM+board.botL==264||
-     board.topL+board.midM+board.botR==264){
-    draw("win"); cout << "Player X is the winner!" << endl << endl; exit(EXIT_SUCCESS);}
-   if(board.topL+board.topM+board.topR==237||board.midL+board.midM+board.midR==237||board.botL+board.botM+
-     board.botR==237||board.topR+board.midR+board.botR==237||board.topM+board.midM+board.botM==237||
-     board.topL+board.midL+board.botL==237||board.topR+board.midM+board.botL==237||
-     board.topL+board.midM+board.botR==237){
-    draw("win"); cout << "Player O is the winner!" << endl << endl; exit(EXIT_SUCCESS);}
-   if(board.topL+board.topM+board.topR+board.midL+board.midM+board.midR+board.botL+board.botM+board.botR==756){
+  if (board.won)
+  {
+    draw ("win"); cout << "Player " << player << " is the winner!" << endl << endl; exit(EXIT_SUCCESS);
+  }
+  // if(board.topL+board.topM+board.topR==264||board.midL+board.midM+board.midR==264||board.botL+board.botM+
+  //    board.botR==264||board.topR+board.midR+board.botR==264||board.topM+board.midM+board.botM==264||
+  //    board.topL+board.midL+board.botL==264||board.topR+board.midM+board.botL==264||
+  //    board.topL+board.midM+board.botR==264){
+  //   draw("win"); cout << "Player X is the winner!" << endl << endl; exit(EXIT_SUCCESS);}
+  //  if(board.topL+board.topM+board.topR==237||board.midL+board.midM+board.midR==237||board.botL+board.botM+
+  //    board.botR==237||board.topR+board.midR+board.botR==237||board.topM+board.midM+board.botM==237||
+  //    board.topL+board.midL+board.botL==237||board.topR+board.midM+board.botL==237||
+  //    board.topL+board.midM+board.botR==237){
+  //   draw("win"); cout << "Player O is the winner!" << endl << endl; exit(EXIT_SUCCESS);}
+   if(board.topleft+board.topmid+board.topright+board.midleft+board.midmid+board.midright+board.botleft+board.botmid+board.botright==756){
      draw("win"); cout << "It's a tie!" << endl << endl; exit(EXIT_SUCCESS);}
   
 }
+
