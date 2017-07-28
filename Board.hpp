35972f1ec8dfe3b0dd763c225ff125b3e4bf91b2
Tommy <tommy.russoniello@gmail.com>
2017-07-27 23:32:43 -0400

    Change board spaces to be stored in char array fix algorithm

--- Board.hpp
+++ Board.hpp
@@ -8,30 +8,29 @@ using std::cout;
 using std::endl;
 using std::vector;
 using std::find;
-//using std::push_back;
+using std::iterator;
 
 class Board
 {
  public :
   bool won;
-  char topleft, topmid, topright, midleft, midmid, midright, botleft, botmid, botright;
+  char spaces [10];
   vector<int> movesX;
   vector<int> movesO;
 
-  Board ()
+  Board () : won (false)
     {
-      topleft = topmid = topright = midleft = midmid = midright = botleft = botmid = botright = ' ';
-      won = false;
+      for (int i = 0; i < 9; ++i) { spaces[i] = ' '; }
     }
 
 void
 print ()
 {
-  cout << endl << " " << topleft << " | " << topmid << " | " << topright << "  " << endl
+  cout << endl << " " << spaces [0] << " | " << spaces [1] << " | " << spaces [2] << "  " << endl
        << "___|___|___" << endl
-       << " " << midleft << " | " << midmid << " | " << midright << " " << endl
+       << " " << spaces [3] << " | " << spaces [4] << " | " << spaces [5] << " " << endl
        << "___|___|___" << endl
-       << " " << botleft << " | " << botmid << " | " << botright << " " << endl
+       << " " << spaces [6] << " | " << spaces [7] << " | " << spaces [8] << " " << endl
        << "   |   |   " << endl << endl;
 }
 
@@ -51,38 +50,37 @@ printSample ()
   {
     switch(pos)
       {
-        case(7): return update(topleft, xo, 1);
-        case(8): return update(topmid, xo, 2);
-        case(9): return update(topright, xo, 3);
-        case(4): return update(midleft, xo, 4);
-        case(5): return update(midmid, xo, 5);
-        case(6): return update(midright, xo, 6);
-        case(1): return update(botleft, xo, 7);
-        case(2): return update(botmid, xo, 8);
-        case(3): return update(botright, xo, 9);
+        case(7): return update(xo, 1);
+        case(8): return update(xo, 2);
+        case(9): return update(xo, 3);
+        case(4): return update(xo, 4);
+        case(5): return update(xo, 5);
+        case(6): return update(xo, 6);
+        case(1): return update(xo, 7);
+        case(2): return update(xo, 8);
+        case(3): return update(xo, 9);
         default: return false; break;
       }
   }
 
 
- private :
+  private :
   
   bool
-  update (char& pos, char xo, int posNum)
+  update (char xo, int pos)
   {
-    if (pos != ' ') { return false; }
-
-    topleft = xo;
+    if (!isspace(spaces[pos - 1])) { return false; }
+    spaces [pos - 1] = xo;
 
     if (xo == 'X')
     {
-      movesX.push_back(posNum);
-      if (movesX.size() >= 3) { hasWon(posNum, movesX); }
+      movesX.push_back(pos);
+      if (movesX.size() >= 3) { hasWon(pos, movesX); }
     }
     else
     {
-      movesO.push_back(posNum);
-      if (movesO.size() >= 3) { hasWon(posNum, movesO); }
+      movesO.push_back(pos);
+      if (movesO.size() >= 3) { hasWon(pos, movesO); }
     }
     return true;
   }
@@ -90,7 +88,7 @@ printSample ()
   void
   hasWon (int pos, vector<int>& moves)
   {
-    if      (!pos & 1) { midCheck (pos, moves);   }
+    if    (!(pos & 1)) { midCheck (pos, moves);   }
     else if (pos != 5) { cornerCheck(pos, moves); }
     else               { centerCheck(moves); }
   }
@@ -98,19 +96,29 @@ printSample ()
   void
   midCheck (int pos, vector<int>& moves)
   {
-    won = (
-      (vfind (moves, pos - 1) && vfind (moves, pos + 1)) ||
-      (vfind (moves, pos - 3) && vfind (moves, pos + 3)) ||
-      (vfind (moves, 5) && vfind (moves, opposite(pos)))
-    );
+    won = true;
+    if (pos == 2 || pos == 8) {
+      if (vfind (moves, pos - 1) && vfind (moves, pos + 1)) { return; }
+    }
+    else
+    {
+      if (vfind (moves, pos - 3) && vfind (moves, pos + 3)) { return; }
+    }
+    won = (vfind (moves, 5) && vfind (moves, opposite(pos)));
   }
 
   void
   cornerCheck (int pos, vector<int>& moves)
   {
+    won = true;
+    if (pos == 3 || pos == 9)
+    {
+      if (vfind (moves, pos - 1) && vfind (moves, pos - 2)) { return; }
+    }
+    else{
+      if (vfind (moves, pos + 1) && vfind (moves, pos + 2)) { return; }
+    }
     won = (
-      (vfind (moves, pos - 1) && vfind (moves, pos - 2)) ||
-      (vfind (moves, pos + 1) && vfind (moves, pos + 2)) ||
       (vfind (moves, pos - 3) && vfind (moves, pos - 6)) ||
       (vfind (moves, pos + 3) && vfind (moves, pos + 6)) ||
       (vfind (moves, 5) && vfind (moves, opposite(pos)))
--- tictactoe.cpp
+++ tictactoe.cpp
@@ -93,7 +93,10 @@ update ()
       break;
     }
 
-    if(!board.setValue(move, player))
+    // convert rom char to corresponding integer
+    int moveNum = move - '0';
+
+    if(!board.setValue(moveNum, player))
     {
       draw ();
       cout << "Space already taken\n";
@@ -129,8 +132,8 @@ hasWon()
   //    board.topL+board.midL+board.botL==237||board.topR+board.midM+board.botL==237||
   //    board.topL+board.midM+board.botR==237){
   //   draw("win"); cout << "Player O is the winner!" << endl << endl; exit(EXIT_SUCCESS);}
-   if(board.topleft+board.topmid+board.topright+board.midleft+board.midmid+board.midright+board.botleft+board.botmid+board.botright==756){
-     draw("win"); cout << "It's a tie!" << endl << endl; exit(EXIT_SUCCESS);}
+  //  if(*board.topleft+*board.topmid+*board.topright+*board.midleft+*board.midmid+*board.midright+*board.botleft+*board.botmid+*board.botright==756){
+  //    draw("win"); cout << "It's a tie!" << endl << endl; exit(EXIT_SUCCESS);}
   
 }
 
